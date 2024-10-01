75967eff0abc9cbba3db0da1a3ed3244ee0a2b46
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-10-01 15:27:45 -0300

    Corrigido rotas publicas e privadas

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../App";
 
 import LoginPage from "../pages/LoginPage/LoginPage";
@@ -8,62 +8,59 @@ import ErroPage from "../pages/ErroPage/ErroPage";
 import DashboardPage from "../pages/DashboardPage/DashboardPage";
 import CadastroLocalPage from "../pages/CadastroLocalPage/CadastroLocalPage";
 import ListaLocalPage from "../pages/ListaLocalPage/ListaLocalPage";
-import { getCookie} from "../hooks/useCookies";
+import { getCookie } from "../hooks/useCookies";
 
 const isAuthenticated = getCookie("usuarioLogado") !== null;
 
 const PrivateRoute = ({ children }) => {
- return isAuthenticated ? children : <LoginPage />;
+    return isAuthenticated ? children : <Navigate to="/" replace={true} />;
 };
 
 const routers = createBrowserRouter([
- {
-  path: "/",
-  element: <LoginPage />
- },
- {
-  path: "/cadastroUsuario",
-  element: <CadastroUsuarioPage />
- },
- {
-  path: "/",
-  element: <App />,
-  errorElement: <ErroPage />,
-  children: [
-   {
-    path: "/dashboard",
-    element: (
-     <PrivateRoute>
-      <DashboardPage />
-     </PrivateRoute>
-    )
-   },
-   {
-    path: "/cadastroLocal",
-    element: (
-     <PrivateRoute>
-      <CadastroLocalPage />
-     </PrivateRoute>
-    )
-   },
-   {
-    path: "/cadastroLocal/:id",
-    element: (
-     <PrivateRoute>
-      <CadastroLocalPage />
-     </PrivateRoute>
-    )
-   },
-   {
-    path: "/listaLocal",
-    element: (
-     <PrivateRoute>
-      <ListaLocalPage />
-     </PrivateRoute>
-    )
-   }
-  ]
- }
+    {
+        path: "/",
+        element: <LoginPage />
+    },
+    {
+        path: "/cadastroUsuario",
+        element: <CadastroUsuarioPage />
+    },
+    {
+        path: "/",
+        element: (
+            <PrivateRoute>
+                <App />,
+            </PrivateRoute>
+        ),
+        errorElement: <ErroPage />,
+        children: [
+            {
+                path: "/dashboard",
+                element:
+                    <DashboardPage />
+
+            },
+            {
+                path: "/cadastroLocal",
+                element:
+                    <CadastroLocalPage />
+
+
+            },
+            {
+                path: "/cadastroLocal/:id",
+                element:
+                    <CadastroLocalPage />
+
+            },
+            {
+                path: "/listaLocal",
+                element:
+                    <ListaLocalPage />
+
+            }
+        ]
+    }
 ]);
 
 export default routers;

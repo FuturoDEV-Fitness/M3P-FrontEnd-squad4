1ef822d565c321e7ab75c8616d0877296459e3ef
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-10-04 19:54:55 -0300

    Componente LayoutComum responsivo, necessário modificar responsividade CardForm e MapaForm

--- src/components/molecules/LayoutComumLocais/LayoutComumLocais.jsx
+++ src/components/molecules/LayoutComumLocais/LayoutComumLocais.jsx
@@ -1,5 +1,4 @@
 import { Badge, Grid, Typography, Button, Box } from "@mui/material";
-import styles from "./LayoutComum.module.css";
 import CardLocalForm from "../CardLocalForm";
 import { useApiLocal } from "../../../hooks/useApiLocal";
 import { useApiUsuario } from "../../../hooks/useApiUsuario";
@@ -7,32 +6,54 @@ import PeopleIcon from '@mui/icons-material/People';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import { Link } from "react-router-dom";
 
-
 function LayoutComum({ titulo, showAuthButtons, visivel }) {
     const { locais, totalLocais } = useApiLocal();
     const { totalOnline } = useApiUsuario();
-
 
     return (
         <Grid
             container
             direction="column"
             alignItems="center"
-            justifyContent="flex-start"  /* Garante que o título fique mais no topo */
-            sx={{ minHeight: "100vh", padding: "20px" }}
-            className={styles.containerPrincipalLayoutComum}
+            justifyContent="flex-start"
+            sx={{
+                minHeight: "100vh",
+                padding: { xs: "10px", md: "20px" },
+                width: "100%",
+                maxWidth: "100%",
+                margin: "60px auto",
+                borderRadius: "10px",
+                overflowX: "hidden",
+            }}
         >
-
-
             {showAuthButtons && (
                 <>
-                    <Typography variant="h3" gutterBottom sx={{ textAlign: 'center' }}>
+                    <Typography
+                        variant="h3"
+                        gutterBottom
+                        sx={{ textAlign: 'center', fontSize: { xs: "2rem", md: "2.5rem" } }}
+                    >
                         Junte-se à nossa rede de pessoas apaixonadas por atividades físicas!
                     </Typography>
-                    <Typography variant="h5" gutterBottom sx={{ textAlign: 'center' }}>
+                    <Typography
+                        variant="h5"
+                        gutterBottom
+                        sx={{ textAlign: 'center', fontSize: { xs: "1.2rem", md: "1.5rem" } }}
+                    >
                         Transforme sua vida com exercícios e alcance seus objetivos de saúde e bem-estar.
                     </Typography>
-                    <Grid className={styles.info}>
+                    <Grid
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            gap: { xs: "10px", md: "20px" },
+                            margin: "20px 0",
+                            flexWrap: "wrap"
+                            // flexDirection: { xs: "column", md: "row" },
+                        }}
+                    >
                         <Badge color="primary" badgeContent={totalOnline} max={999}>
                             <PeopleIcon sx={{ color: "black", fontSize: 35 }} />
                         </Badge>
@@ -41,38 +62,76 @@ function LayoutComum({ titulo, showAuthButtons, visivel }) {
                         </Badge>
                     </Grid>
 
-
-                    <Box mt={3}>
-                        <Grid className={styles.authButtons}>
+                    <Box mt={3} sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                        <Grid
+                            sx={{
+                                display: "flex",
+                                gap: { xs: "10px", md: "15px" },
+                                flexDirection: { xs: "column", sm: "row" },
+                                width: { xs: "auto", md: "60%" },
+                                justifyContent: "center",
+                                marginBottom: { xs: "20px", md: "30px" },
+                            }}
+                        >
                             <Link to={`/cadastroUsuario`} style={{ textDecoration: 'none' }}>
-
-                                <Button variant="contained" color="primary">Cadastre-se</Button>
+                                <Button variant="contained" color="primary" fullWidth>
+                                    Cadastre-se
+                                </Button>
                             </Link>
                             <Link to={`/login`} style={{ textDecoration: 'none' }}>
-
-                                <Button variant="outlined" color="primary">Login</Button>
+                                <Button variant="outlined" color="primary" fullWidth>
+                                    Login
+                                </Button>
                             </Link>
                         </Grid>
                     </Box>
                 </>
-            )
-            }
+            )}
 
-            <Typography variant="h4" className={styles.titulo}>{titulo}</Typography>
+            <Typography
+                variant="h4"
+                sx={{ fontSize: { xs: "1.5rem", md: "2rem", xl: "3rem" }, textAlign: "center", margin: "20px 0" }}
+            >
+                {titulo}
+            </Typography>
 
             <Grid
-                sx={{ flexDirection: "column" }}
-                className={styles.containerLayoutListaLocais}>
-                <Grid sx={{ flexDirection: "column" }} className={styles.containerCards}>
-                    {/* <Typography className={styles.titulo}>Locais incríveis</Typography> */}
-                    {locais.map(
-                        (local, index) => (
-                            console.log(local), (<CardLocalForm dadosLocal={local} key={index} visivel={visivel} />)
-                        )
-                    )}
-              </Grid>
+                container
+                direction="column"
+                alignItems="center"
+                sx={{
+                    marginTop: "60px",
+                    width: "100%",
+                    // backgroundColor: "#e5e5e5",
+                    zIndex: 1,
+                }}
+            >
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    sx={{
+                        width: "100%",
+                        maxWidth: "1200px",
+                        padding: { xs: "10px", sm: "20px" },
+                        gap: { xs: "10px", md: "20px" },
+                    }}
+                >
+                    {locais.map((local, index) => (
+                        <CardLocalForm
+                            dadosLocal={local}
+                            key={index}
+                            visivel={visivel}
+                            sx={{
+                                width: "100%",
+                                maxWidth: { xs: "100%", md: "600px" },
+                                margin: { xs: "10px 0", md: "20px 0" },
+                            }}
+                        />
+                    ))}
+                </Grid>
             </Grid>
-        </Grid >
+        </Grid>
     );
 }
 
--- src/components/molecules/MapaForm/index.jsx
+++ src/components/molecules/MapaForm/index.jsx
@@ -3,26 +3,26 @@ import { Grid } from "@mui/material";
 import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
 
 function MapaForm(dadosLocal) {
- const mapConfig = {
-  lat: dadosLocal.latitude,
-  lng: dadosLocal.longitude,
-  zoom: 10
- };
+    const mapConfig = {
+        lat: dadosLocal.latitude,
+        lng: dadosLocal.longitude,
+        zoom: 10
+    };
 
- return (
-  <Grid className="containerMapa">
+    return (
+        <Grid className="containerMapa">
    // Create a map instance and set the initial view coordinates and zoom level
-   <MapContainer
-    center={[mapConfig.lat, mapConfig.lng]}
-    zoom={mapConfig.zoom}
-    style={{ height: "400px" }}>
-    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    <Marker position={[mapConfig.lat, mapConfig.lng]}>
-     <Popup>A popup message on the marker.</Popup>
-    </Marker>
-   </MapContainer>
-  </Grid>
- );
+            <MapContainer
+                center={[mapConfig.lat, mapConfig.lng]}
+                zoom={mapConfig.zoom}
+                style={{ height: "400px" }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={[mapConfig.lat, mapConfig.lng]}>
+                    <Popup>A popup message on the marker.</Popup>
+                </Marker>
+            </MapContainer>
+        </Grid>
+    );
 }
 
 export default MapaForm;
